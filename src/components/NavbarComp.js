09272bd6b309f37a9c98d7149d3279d67c81fed1
Rodrigo Ligeiro <rrligeiro@gmail.com>
2022-10-30 13:11:55 -0300

    Update on navbar and footer

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -1,16 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../images/logo.png';
 
 function NavbarComp() {
   return (
-    <Navbar bg="dark" variant="dark" fixed="top">
+    <Navbar variant="light" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="Destinos logo"
+          />
+        </Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
